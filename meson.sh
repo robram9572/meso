24a2dfc36028273637dadbb8b1386e2ac348de27
robram <34120952+robram9572@users.noreply.github.com>
2024-01-07 14:24:00 +0800

    Update meson.sh

--- meson.sh
+++ meson.sh
@@ -12,16 +12,6 @@ sleep 20
 
 sleep 20
 
-sudo ./apps/gaganode/gaganode config set --token=ipjahthgyjuoakdeb7584862158397c5
-#sudo ./apps/gaganode/gaganode config set --token=qygidhtldnsuhmvl50a1539a5dbf011e
-#sudo ./apps/gaganode/gaganode config set --token=dfqwmjnncpfyzytodd0cec9a5041b044
-#sudo ./apps/gaganode/gaganode config set --token=vlkrrblzzujryhxj55b26f77f58d2b25
-#sudo ./apps/gaganode/gaganode config set --token=dflljhsoaimvwlfo9e38d2fe8cbd9a12
-#sudo ./apps/gaganode/gaganode config set --token=khlhdlxyakkuvvvq0d51f199873c030c
-#sudo ./apps/gaganode/gaganode config set --token=twwxxrfohpmnbchec164d236899f7479
-#sudo ./apps/gaganode/gaganode config set --token=hddilqxvvhhnznkf59f697bfa146c2b2
-#sudo ./apps/gaganode/gaganode config set --token=ygjbjxiaxxidhhzy3c74604866b63480
-#sudo ./apps/gaganode/gaganode config set --token=pjjkuesmrtwllogt3e2ff35375df38f0
-#sudo ./apps/gaganode/gaganode config set --token=rmeinhjrsrkwqslp72448981775fd38c
+
 
 ./apphub restart
